2c731ea91fab30e780d67be625fe2f22a33a8ce6
ryuyauetake <ryuya@anymindgroup.com>
2021-07-20 00:18:28 +0900

    Feat display text to chatContent

--- my-app/src/component/orgs/chatSlider/ChatSlider.tsx
+++ my-app/src/component/orgs/chatSlider/ChatSlider.tsx
@@ -1,25 +1,34 @@
 // @flow
 import * as React from 'react';
-import {useContext, useState} from 'react';
-import { chatContext, Status } from '../../abstracts/chatContext/ChatContext';
+import {useContext, useEffect, useState} from 'react';
+import {chatContext, Status} from '../../abstracts/chatContext/ChatContext';
 import './ChatSlider.css';
 
 export const ChatSlider = () => {
   const chatCxt = useContext(chatContext)
   const [isActive, setActive] = useState(false);
+  const [chatContent, chatUpdate] = useState("");
+
+  useEffect(() => chatUpdate("Hello World"),[])
 
   const toggleChatSlider = () => {
-    chatCxt.update(chatCxt.current === Status.CLOSE ? Status.OPEN :Status.CLOSE)
+    chatCxt.update(chatCxt.current === Status.CLOSE ? Status.OPEN : Status.CLOSE)
+  };
+  const toggleChatActive = () => {
     setActive(!isActive);
   };
 
   return (
     <div className="spa" id="spa">
       <div
-        onClick={toggleChatSlider}
+        onClick={() => {
+          toggleChatSlider();
+          toggleChatActive();
+        }}
         className={`spa-slider ${isActive ? "isOpen" : "isClose"}`}
-        title={isActive ? "Click to extend" : ""}>
-        <p>hello world</p>
+        title={isActive ? "Click to extend" : ""}
+      >
+        <p>{chatContent}</p>
       </div>
     </div>
   );
